6788f96223beaae1060e9aa14d1f01025275f342
gosgn <gosgnach24lucas@gmail.com>
2024-03-05 15:51:32 +0100

    Update V1.1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -133,28 +133,6 @@ const NavBar = ({menu, setMenu, setIsSetting, setDevice}) =>{
                     </svg>
 
                 </a>
-                {/* Settings */}
-                <a
-                    href="#"
-                    className="cursor-pointer rounded-lg p-1.5 text-slate-500 transition-colors duration-200 hover:bg-slate-200 focus:outline-none dark:text-slate-400 dark:hover:bg-slate-800"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        viewBox="0 0 24 24"
-                        strokeWidth="2"
-                        stroke="currentColor"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                        <path
-                            d="M19.875 6.27a2.225 2.225 0 0 1 1.125 1.948v7.284c0 .809 -.443 1.555 -1.158 1.948l-6.75 4.27a2.269 2.269 0 0 1 -2.184 0l-6.75 -4.27a2.225 2.225 0 0 1 -1.158 -1.948v-7.285c0 -.809 .443 -1.554 1.158 -1.947l6.75 -3.98a2.33 2.33 0 0 1 2.25 0l6.75 3.98h-.033z"
-                        ></path>
-                        <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-                    </svg>
-                </a>
             </div>
             {/* Second Column */}
             <MenuLeft menu={menu} setIsSetting={setIsSetting} setDevice={setDevice}/>
