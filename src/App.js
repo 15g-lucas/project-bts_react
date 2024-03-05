8d55b1261074bc6b7ba6e11715344c0315d9050f
gosgn <gosgnach24lucas@gmail.com>
2024-03-05 10:58:10 +0100

    Update V1.1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from "./components/NavBar";
 import SettingsBar from "./components/SettingsBar";
 import LineChart from "./components/LineChart";
 import Automaton from "./components/Automaton";
+import Variables from "./components/Variables";
 
 function App() {
     const [menu, setMenu] = useState(1)
@@ -23,6 +24,8 @@ function App() {
                     switch (menu) {
                         case 1 :
                             return <Automaton device={device}/>
+                        case 2 :
+                            return <Variables />
                         case 3:
                             return <LineChart />
                     }
